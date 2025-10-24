ee1870b81d2a44c027095161c272bbda5b19aff5
xdalp <mgarbe744@gmail.com>
2025-10-24 16:53:36 +0000

    Méthode récupération edt (en cours...)

--- extraction_pamplemousse.py
+++ extraction_pamplemousse.py
@@ -1,6 +1,8 @@
 #Ce code permet de récupérer l'edt des salles pamplemousse
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta 
+import json
 
 class User:
     def __init__(self,identifiant : str, mdp : str):
@@ -10,7 +12,7 @@ class User:
         self.autent=False
     
     def connexion(self):
-        url_page ="https://pamplemousse.ensae.fr/index.php?p=400"
+        url_page ="https://pamplemousse.ensae.fr"
         resp_page =self.session.get(url_page)
         soup = BeautifulSoup(resp_page.text, 'html.parser')
         sph_org_location =soup.find('input', {'name': 'sph_org_location'})['value']  #scraping sur la page source pamplemousse
@@ -34,3 +36,37 @@ class User:
             self.autent=False
             print("Echec de la connexion à Rutabaga. Identifiant ou mot de passe incorrect.")
         return self.autent
+
+    #LA METHODE NE FONCTIONNE PAS ENCORE (renvoie une liste vide), IL FAUT TROUVER LE ENDPOINT JSON de "EDT - TOUT" sur PAMPLEMOUSSE 
+    def edt(self,duree=1,start : datetime =None):
+        if not self.autent:
+            print("Tentative de connexion à Rutabaga via Pamplemousse...")
+            self.connexion()
+        if not self.autent:
+            print("Impossible de se connecter. Edt inaccessible")
+            return None
+        
+        url_backend="https://pamplemousse.ensae.fr/index.php?p=4040" #backend
+        url_front="https://pamplemousse.ensae.fr/index.php?p=404"
+        headers={
+        "User-Agent": "Mozilla/5.0",
+        #"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Referer": url_front,}
+
+        if start is None:
+            start = datetime.now()  #maintenant
+        end= start + timedelta(hours=duree)
+        start = start.strftime("%Y-%m-%d %H:%M")
+        end=end.strftime("%Y-%m-%d %H:%M")
+
+        data= {"start": start, "end": end}
+
+        resp = self.session.post(url_backend, headers=headers, data=data)
+        if resp.status_code == 200:
+            print("Rutabaga a accédé à l'Edt avec succès !")
+            return json.loads(resp.text)
+        else:
+            print(f"Rutabaga ne parvient pas à récupérer l'Edt : {resp.status_code}")
+            return None
+
