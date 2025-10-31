ce3513347c527fe067c468b1782820264e9624c7
mgarbe <mateo.garbe@ensae.fr>
2025-10-31 17:00:36 +0000

    Initialisation d'une classe Salle

--- classes.py
+++ classes.py
@@ -144,3 +144,44 @@ class User:
         liste_salles = [s for s in gdf['label'].tolist() if s is not None and re.search(r'\d', s)]
         liste_salles_libres = [s for s in liste_salles if s not in salles_occ]
         return liste_salles_libres
+
+
+
+class Salle:
+     """
+    La classe Salle permet de calculer des informations sur les salles
+    """
+
+    def __init__(self,label : str):
+        """
+        Initialisation de la classe User.
+        Args:
+            label (str) : nom de la salle
+        """
+        self.label=label
+        self.geometry=None
+        #ajouter la géométrie !
+    
+    def distance(self, objet : str):
+        """
+        Idée : pour un objet donné on calcul la distance de la salle à l'objet 
+        cf. Isssues
+        """
+    
+    def projection_couloir(self):
+        """
+        Projection dans le couloir pour calcul des déplacements
+        """
+
+    def capacite(self):
+        """
+        Estimation capacité avec l'aire (trouver un ratio nb places / m2)
+        """
+
+    def info(self):
+        """
+        detection si salle info : teste la présence d'un i dans le label
+        """
+    
+
+    
